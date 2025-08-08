82e6c9fdfa97330a00d8ef403fd37dc7c77ad36b
Eleomar Fajutnao <109301348+Pabyala@users.noreply.github.com>
2025-08-08 20:14:40 +0800

    added: deleteProduct controller

--- backend/src/controllers/product-controllers.ts
+++ backend/src/controllers/product-controllers.ts
@@ -9,7 +9,8 @@ export const getAllProducts = async (req: Request, res: Response) => {
         .populate('updatedBy', 'name email');
 
         res.status(200).json({ 
-            message: true, 
+            success: true, 
+            message: 'All products.',
             data: user
         })
     } catch (error) {
@@ -24,7 +25,10 @@ export const newProductItem = async (req: Request, res: Response) => {
     const { name, price, description, image, currency } = req.body;
     try {
         if(!name || !price || !description) {
-            return res.status(400).json({ success: false, message: 'Name, price, and description are required.' })
+            return res.status(400).json({ 
+                success: false, 
+                message: 'Name, price, and description are required.' 
+            })
         }
 
         const newProduct = new Product({
@@ -101,5 +105,37 @@ export const updateProduct = async (req: Request, res: Response) => {
     } catch (error) {
         console.error("Error during sign-up:", error);
         res.status(500).send({ success: false, message: "Internal server error" });
+    }
+}
+
+export const deleteProduct = async (req: Request, res: Response) => {
+    const productId = req.params.productId;
+    try {
+        if (!Types.ObjectId.isValid(productId)) {
+            return res.status(400).json({ 
+                success: false, message: 
+                'Invalid product ID' 
+            });
+        }
+
+        const product = await Product.findByIdAndDelete(productId)
+
+        if (!product) {
+            return res.status(404).json({ 
+                message: 'Product not found.' 
+            });
+        }
+        
+        res.status(200).json({ 
+            success: true, 
+            message: "Product deleted successfully." 
+        })
+
+    } catch (error) {
+        console.error("Error during sign-up:", error);
+        res.status(500).send({ 
+            success: false, 
+            message: "Internal server error" 
+        });
     }
 }
--- backend/src/routes/api/product-api.ts
+++ backend/src/routes/api/product-api.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { getAllProducts, newProductItem, updateProduct } from "../../controllers/product-controllers";
+import { deleteProduct, getAllProducts, newProductItem, updateProduct } from "../../controllers/product-controllers";
 import { verifyJWT } from "../../middlewares/verify-jwt";
 const router = Router();
 
-router.get("/all", getAllProducts);
+router.get("/all", verifyJWT, getAllProducts);
 router.post("/new", verifyJWT, newProductItem);
 router.put("/update/:productId", verifyJWT, updateProduct);
+router.delete("/delete/:productId", verifyJWT, deleteProduct);
 
 export default router;
